d9c053a19d9990627f75a0482164fee33c30a9be
Matthew Braun <mbraun5@csu.fullerton.edu>
2020-03-02 00:57:08 -0800

    force navbar to show if at bottom of page

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -2,7 +2,9 @@ var prevScrollpos = window.pageYOffset;
 
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
+
+  if ((prevScrollpos > currentScrollPos) || 
+    (window.innerHeight + window.pageYOffset) >= document.body.offsetHeight) {
     document.getElementById("header").style.top = "0";
   } else {
     if (currentScrollPos > 275){
