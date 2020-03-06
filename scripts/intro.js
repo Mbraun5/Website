d1672f1dfd17d28b12930a2c91d8a3f6988b8949
Matthew Braun <mbraun5@csu.fullerton.edu>
2020-03-06 12:37:30 -0800

    added animation sequence for intro

--- scripts/intro.js
+++ scripts/intro.js
@@ -3,25 +3,55 @@ var animations = [
     ["#intro1", 700],
     ["#intro2", 700],
     ["#intro3", 700],
-    ["#intro4", 700],
 ]
 
 function animate_opacity(id, time, callback) {
     $(id).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1}, time, callback);
 }
 
-function animate_intro(index) {
+function animate_intro(index, callback) {
     if (index < animations.length) {
         animate_opacity(
             animations[index][0], 
             animations[index][1], 
             function() {
-                animate_intro(index+1);
+                animate_intro(index+1, callback);
             })
+    }
+    if (callback && (index == 2)) {
+      callback();
     }
 }
 
-animate_intro(0);
+function animate_intro4() {
+  //var text_one = $("<")"I have a B.A. in Computer Science and a B.S. in Mathematics, and I love"
+  //var text_two = "solving problems."
+  var $full_text = $('#intro4');
+  $full_text[0].innerHTML = $full_text[0].innerHTML.replace(/(\r\n|\n|\r)/gm, "").trim().replace(/\s{2,}/, " ").replace(/<(.*?)>/, "!");
+  var $wordList = $('#intro4').html().split("");
+  console.log($wordList)
+  $('#intro4').html("");
+  var cur_class = "";
+  $.each($wordList, function(idx, elem) {
+    if (elem == "!") {
+      cur_class = "emphasis"
+    } else {
+        var newEL = $("<span/>").text(elem).css({
+          opacity: 0
+        }).addClass(cur_class);
+        newEL.appendTo($full_text);
+        newEL.delay(idx * 70);
+        newEL.animate({
+          opacity: 1
+        }, 700);
+      }
+   })
+}
+
+animate_intro(0, function() {
+  $("#intro4").css({opacity: 1});
+  animate_intro4()
+});
 
 /*
 $(function() {
@@ -79,7 +109,7 @@ $(function() {
     });
   });
 */
-
+  /*
   $(function() {
     var message = document.getElementById("intro4").innerHTML
     $('#intro4').html("");
@@ -105,3 +135,4 @@ $(function() {
     }
     var repeat = setInterval(displayLetter, 60);
   });
+  */
