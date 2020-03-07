69811cc8bb25cfd6e890bd82c8b9bfc63fe7b567
Matthew Braun <mbraun5@csu.fullerton.edu>
2020-03-07 15:46:29 -0800

    added animation for about section

--- scripts/animate_body.js
+++ scripts/animate_body.js
@@ -1,19 +1,42 @@
-var about_done = true;
+var about_done = false;
+var about_text_done = false;
+var about_img_done = false;
 
 function animate_about() {
-    if (!about_done) {
+    if (about_done) {
         return;
     }
-
     var scroll_bottom = window.innerHeight + window.pageYOffset;
-    var about_top = $("#about").offset().top + (Math.ceil(window.innerHeight / 2))
+    animate_about_text(scroll_bottom);
+    animate_about_img(scroll_bottom);
+}
 
-    console.log(scroll_bottom);
-    console.log(about_top);
-
-    if (scroll_bottom >= about_top) {
-        alert("HERE");
-        about_done = false;
+function animate_about_text(scroll_bottom) {
+    if (about_text_done) {
+        if (about_img_done) {
+            about_done = true;
+        }
+        return;
     }
-    console.log("here");
+    var text_top = $(".about-text").offset().top + (Math.ceil(window.innerHeight / 3))
+
+    if (scroll_bottom >= text_top) {
+        about_text_done = true;
+        $(".about-text").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1}, 1500);
+    }
+}
+
+function animate_about_img(scroll_bottom) {
+    if (about_img_done) {
+        if (about_text_done) {
+            about_done = true;
+        }
+        return;
+    }
+    var img_top = $("#about-img").offset().top + (Math.ceil(window.innerHeight / 3))
+
+    if (scroll_bottom >= img_top) {
+        about_img_done = true;
+        $("#about-img").css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1}, 1500);
+    }
 }
