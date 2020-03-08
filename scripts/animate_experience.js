3101229d8966d79c905debbe6192a23b499ea65c
Matthew Braun <mbraun5@csu.fullerton.edu>
2020-03-07 17:31:58 -0800

    moved experience animations to proper javascript file

--- scripts/animate_experience.js
+++ scripts/animate_experience.js
@@ -22,3 +22,21 @@ function animate_experience_title(scroll_bottom) {
         $("#experience-hr").animate({opacity: 1}, {queue: false, duration: 1500});
     }    
 }
+
+var active_button = document.getElementById("experience-btn-group").children[0];
+var active_text = document.getElementById("experience-txt-group").children[0];
+active_button.classList.toggle("active-button");
+active_text.classList.toggle("active-text");
+
+function toggle_active(btn, idx) {
+    if (btn != active_button) {
+        active_button.classList.toggle("active-button");
+        btn.classList.toggle("active-button");
+
+        active_text.classList.toggle("active-text")
+        active_text = document.getElementById("experience-txt-group").children[idx];
+        active_text.classList.toggle("active-text");
+
+        active_button = btn;
+    }
+}
